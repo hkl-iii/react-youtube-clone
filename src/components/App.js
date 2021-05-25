2610c58dcf71eacd555c94dcc01894d0ee0a7cd5
hkl-iii <rahleggon@gmail.com>
2021-05-25 16:56:40 -0500

    copied and pasted layout from reactbootstrap inside return() in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   
+    <Container>
+    <Row>
+      <Col>1 of 2</Col>
+      <Col>2 of 2</Col>
+    </Row>
+    <Row>
+      <Col>1 of 3</Col>
+      <Col>2 of 3</Col>
+      <Col>3 of 3</Col>
+    </Row>
+  </Container>
   );
 }
 
