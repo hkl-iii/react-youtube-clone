78e9de09664fb447333c7013f9c9838b7336822a
hkl-iii <rahleggon@gmail.com>
2021-05-25 17:45:50 -0500

    added button and form code into appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Container>
       <Row>
-        <Col style={{border:"2px solid red"}}>Search</Col>
+        <Col style={{border:"2px solid red"}}>
+        <Form>
+  <Form.Row>
+    <Col>
+      <Form.Control size="lg" type="text" placeholder="Search video here..." />
+    </Col>
+    <Col>
+    <Button variant="primary" size="lg">
+          Search
+        </Button>    
+      </Col>
+  </Form.Row>
+</Form>
+        <Form.Control size="lg" type="text" placeholder="Large text" />
+        </Col>
       </Row>
       <Row>
         <Col xs={12} lg={8} style={{border:"2px solid red"}}>Video</Col>
