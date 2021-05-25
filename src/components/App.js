62199e98a90200249d9652e0ef2344085466808b
hkl-iii <rahleggon@gmail.com>
2021-05-25 17:24:25 -0500

    App.js Fixed up columns lines 9,12,13

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Container>
-    <Row>
-      <Col>1 of 2</Col>
-      <Col>2 of 2</Col>
-    </Row>
-    <Row>
-      <Col>1 of 3</Col>
-      <Col>2 of 3</Col>
-      <Col>3 of 3</Col>
-    </Row>
+      <Row>
+        <Col style={{border:"2px solid red"}}>Search</Col>
+      </Row>
+      <Row>
+        <Col xs={12} lg={8} style={{border:"2px solid red"}}>Video</Col>
+        <Col xs={12} lg={4} style={{border:"2px solid red"}}>Suggestions</Col>
+      </Row>
   </Container>
   );
 }
