fb6ad79432671ac6898626c86fa1f3b2003a5466
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-26 13:44:27 -0500

    updated api.js

---
+++ src/ApiKeys.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBMZV1ctnvz_l4iSqmXx5ldKDb6FR0h7t0'
--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './ApiKeys.js';
+import axios from 'axios';
 
 //  api KEY AIzaSyBMZV1ctnvz_l4iSqmXx5ldKDb6FR0h7t0
+axios.get('https://www.googleapis.com/youtube/v3/search?q={superheroes}&key={KEY}')
 
 function App() {
   return (
@@ -27,6 +30,7 @@ function App() {
       </Row>
       <Row>
         <Col xs={12} lg={8} style={{border:"2px solid red"}}>Video</Col>
+
         <Col xs={12} lg={4} style={{border:"2px solid red"}}>Suggestions</Col>
       </Row>
   </Container>
