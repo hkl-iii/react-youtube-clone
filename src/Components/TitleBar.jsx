7c867602d400c71bb3c47357010d4498c45b2930
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-26 10:19:51 -0500

    Danny Test Commit

---
+++ src/Components/TitleBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class TitleBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>
+                Test
+            </h1>
+         );
+    }
+}
+ 
+export default TitleBar;
